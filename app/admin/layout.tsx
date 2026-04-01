1a2cfdef2cf1efbfeef8296a6bc815db5769c943
milkyano-cpu <dummyforfadedline@gmail.com>
2026-04-01 09:52:45 +0700

    Update : Admin Page

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,3 +1,6 @@
+import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar"
+import { SiteHeader } from "@/components/site-header"
 import { TooltipProvider } from "@/components/ui/tooltip"
 
 export default function AdminLayout({
@@ -5,5 +8,26 @@ export default function AdminLayout({
 }: {
   children: React.ReactNode
 }) {
-  return <TooltipProvider>{children}</TooltipProvider>
+  return (
+    <TooltipProvider>
+      <SidebarProvider
+        style={
+          {
+            "--sidebar-width": "calc(var(--spacing) * 72)",
+          } as React.CSSProperties
+        }
+      >
+        <AppSidebar />
+
+      <SidebarInset>
+        {/* 🔥 HEADER HANYA MOBILE */}
+        <div className="md:hidden">
+          <SiteHeader />
+        </div>
+
+        <div className="p-6">{children}</div>
+      </SidebarInset>
+      </SidebarProvider>
+    </TooltipProvider>
+  )
 }
