220d961f0d8c150c54d374b958dd52253580f3ce
milkyano-cpu <dummyforfadedline@gmail.com>
2026-04-29 07:25:44 +0700

    Update : Videso Hero

--- app/home-client.tsx
+++ app/home-client.tsx
@@ -49,7 +49,7 @@ export default function HomeClient({ projects }: { projects: ProjectCardData[] }
         className="
           relative 
           z-20 
-          -mt-[12vh] md:-mt-[0vh]
+          -mt-[10vh] md:-mt-[0vh]
           bg-white md:bg-[#f5f5f5]
           rounded-t-[20px] md:rounded-t-[30px]
           pt-12 md:pt-16 
