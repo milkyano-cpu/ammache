c585511ea8eae1b3a0689fa11be18cdbe8fa26ad
milkyano-cpu <dummyforfadedline@gmail.com>
2026-04-30 14:11:52 +0700

    Update : Divider

--- components/layouts/footer.tsx
+++ components/layouts/footer.tsx
@@ -76,7 +76,7 @@ const Footer = () => {
               )}
             </div>
 
-            <div className="border-t border-black my-8 -mx-6" />
+            <div className="border-t border-gray-300 my-8 -mx-6" />
 
             {/* LOGO + DESC */}
             <div className="text-center">
